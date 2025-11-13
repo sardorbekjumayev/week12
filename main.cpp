d1f97f6ece7937b177353fd09c4f9c21c732b3f6
sardorbekjumayev <sardorbekjumayev21032008@gmail.com>
2025-11-13 19:21:41 +0500

    update

--- main.cpp
+++ main.cpp
@@ -75,7 +75,20 @@ int main() {
 
     //problem4
 
+int main() {
+    int arr[] = {10, 20, 30, 40, 50}; 
+    int* ptr = arr; 
 
+    cout << "Pointer array elements: ";
+
+    
+    for (int i = 0; i < 5; i++) {
+        cout << *(ptr + i) << " "; 
+    }
+
+    cout << endl;
+    return 0;
+}
 
 
 
